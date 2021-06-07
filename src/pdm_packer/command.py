c30d5f94a2344d866bb184c37d7eb4d894b0a563
Frost Ming <mianghong@gmail.com>
2021-06-07 16:18:11 +0800

    File content are written into the file object instead of memory Close #4

--- src/pdm_packer/command.py
+++ src/pdm_packer/command.py
@@ -1,6 +1,5 @@
 import argparse
 import importlib.resources
-import io
 import os
 import stat
 import zipapp
@@ -8,7 +7,6 @@ from pathlib import Path
 
 from pdm import BaseCommand, Project, termui
 from pdm.models.in_process import get_architecture
-from pip._vendor.pkg_resources import to_filename
 
 from .env import PackEnvironment
 
@@ -46,34 +44,6 @@ class PackCommand(BaseCommand):
             "the file name will end with .exe(Windows) or no suffix(Posix)",
         )
 
-    @staticmethod
-    def _write_zipapp(
-        stream: io.BytesIO, project: Project, options: argparse.Namespace
-    ) -> Path:
-        bytes = stream.getvalue()
-        if options.exe and (
-            os.name == "nt" or (os.name == "java" and os._name == "nt")
-        ):
-            interpreter = options.interpreter or project.python.executable
-            arch = get_architecture(interpreter)
-            bits = "32" if "32bit" in arch else "64"
-            kind = "w" if "pythonw" in Path(interpreter).name else "t"
-            launcher = importlib.resources.read_binary("distlib", f"{kind}{bits}.exe")
-            bytes = launcher + bytes
-
-        if options.output:
-            output = options.output
-        else:
-            name = project.meta.name or project.root.name
-            name = to_filename(name)
-            suffix = ".pyz" if not options.exe else ".exe" if os.name == "nt" else ""
-            output = Path(name + suffix)
-
-        output.write_bytes(bytes)
-        if options.exe:
-            output.chmod(output.stat().st_mode | stat.S_IEXEC)
-        return output
-
     def handle(self, project: Project, options: argparse.Namespace) -> None:
         def file_filter(name: str) -> bool:
             first = Path(name).parts[0]
@@ -93,20 +63,40 @@ class PackCommand(BaseCommand):
             if scripts:
                 main = str(scripts[next(iter(scripts))])
 
-        target_stream = io.BytesIO()
+        if options.output:
+            output = options.output
+        else:
+            name = project.meta.name or project.root.name
+            name = name.replace("-", "_")
+            suffix = ".pyz" if not options.exe else ".exe" if os.name == "nt" else ""
+            output = Path(name + suffix)
 
-        with PackEnvironment(project) as pack_env:
+        with PackEnvironment(project) as pack_env, output.open("wb") as stream:
             project.core.ui.echo("Packing packages...")
             lib = pack_env.prepare_lib_for_pack()
             project.core.ui.echo(f"Packages are prepared at {lib}")
             project.core.ui.echo("Creating zipapp...")
+            if options.exe and (
+                os.name == "nt" or (os.name == "java" and os._name == "nt")
+            ):
+                interpreter = options.interpreter or project.python.executable
+                arch = get_architecture(interpreter)
+                bits = "32" if "32bit" in arch else "64"
+                kind = "w" if "pythonw" in Path(interpreter).name else "t"
+                launcher = importlib.resources.read_binary(
+                    "distlib", f"{kind}{bits}.exe"
+                )
+                stream.write(launcher)
+
             zipapp.create_archive(
                 lib,
-                target_stream,
+                stream,
                 interpreter=options.interpreter or project.python.executable,
                 main=main,
                 compressed=options.compress,
                 filter=file_filter,
             )
-            output = self._write_zipapp(target_stream, project, options)
-            project.core.ui.echo(f"Zipapp is generated at {termui.green(output)}")
+
+        if options.exe:
+            output.chmod(output.stat().st_mode | stat.S_IEXEC)
+        project.core.ui.echo(f"Zipapp is generated at {termui.green(str(output))}")
