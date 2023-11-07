a3e916098daab5a0f547601957b2a69749bb8001
Frost Ming <me@frostming.com>
2023-11-07 15:56:21 +0800

    chore: update workflow to drop 3.7
    Signed-off-by: Frost Ming <me@frostming.com>

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 
 
-@nox.session(python=("3.7", "3.8", "3.9"))
+@nox.session(python=("3.8", "3.9", "3.10", "3.11", "3.12"))
 def test(session):
     session.run("pdm", "install", "-d", external=True)
     session.run("pytest", "tests/")
