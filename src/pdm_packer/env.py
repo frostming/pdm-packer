e8a0e4f47b41572562b66e42dc22898f2fb5fe7e
franzhaas <franz.dominik.haas@gmail.com>
2024-02-04 03:09:16 +0100

    fix: Use importlib.resources to get in-process script (#39)
    * - removed __file__ usage use importlib instead...
    
    * - support 3.8 python...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * - use if instead of exception with catch all...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * - fixed mistake with as file...
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: franz haas <franz.haas@ams-osram.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Frost Ming <mianghong@gmail.com>

--- src/pdm_packer/env.py
+++ src/pdm_packer/env.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import importlib.resources
 import subprocess
+import sys
 from functools import cached_property
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -14,7 +16,13 @@ try:
 except ImportError:
     from pdm.models.environment import Environment as BaseEnvironment
 
-IN_PROCESS_SCRIPT = Path(__file__).with_name("_compile_source.py")
+
+def get_in_process_script():
+    if sys.version_info >= (3, 9):
+        script = importlib.resources.files("pdm_packer") / "_compile_source.py"
+        return importlib.resources.as_file(script)
+    else:
+        return importlib.resources.path("pdm_packer", "_compile_source.py")
 
 
 class PackEnvironment(BaseEnvironment):
@@ -33,8 +41,9 @@ class PackEnvironment(BaseEnvironment):
         self._dir.cleanup()
 
     def _compile_to_pyc(self, dest: Path) -> None:
-        args = [str(self.interpreter.path), str(IN_PROCESS_SCRIPT), str(dest)]
-        subprocess.check_output(args, stderr=subprocess.STDOUT)
+        with get_in_process_script() as scriptpath:
+            args = [str(self.interpreter.path), str(scriptpath), str(dest)]
+            subprocess.check_output(args, stderr=subprocess.STDOUT)
 
     def prepare_lib_for_pack(self, compile: bool = False) -> Path:
         """Get a lib path containing all dependencies for pack.
